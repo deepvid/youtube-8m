323e83414b1db785ffdea3db0c3a9602f5651224
rajgot <rajgot@gmail.com>
2017-05-17 00:33:09 +0530

    MoE: Made it two levels

--- video_level_models.py
+++ video_level_models.py
@@ -75,19 +75,32 @@ class MoeModel(models.BaseModel):
     """
     num_mixtures = num_mixtures or FLAGS.moe_num_mixtures
 
-    gate_activations = slim.fully_connected(
+    gate_activations1 = slim.fully_connected(
         model_input,
         vocab_size * (num_mixtures + 1),
         activation_fn=None,
         biases_initializer=None,
         weights_regularizer=slim.l2_regularizer(l2_penalty),
-        scope="gates")
-    expert_activations = slim.fully_connected(
+        scope="gates1")
+    expert_activations1 = slim.fully_connected(
         model_input,
         vocab_size * num_mixtures,
         activation_fn=None,
         weights_regularizer=slim.l2_regularizer(l2_penalty),
-        scope="experts")
+        scope="experts1")
+    gate_activations2 = slim.fully_connected(
+        model_input,
+        vocab_size * (num_mixtures + 1),
+        activation_fn=None,
+        biases_initializer=None,
+        weights_regularizer=slim.l2_regularizer(l2_penalty),
+        scope="gates2")
+    expert_activations2 = slim.fully_connected(
+        expert_activations1,
+        vocab_size * num_mixtures,
+        activation_fn=None,
+        weights_regularizer=slim.l2_regularizer(l2_penalty),
+        scope="experts2")
 
     gating_distribution = tf.nn.softmax(tf.reshape(
         gate_activations,
